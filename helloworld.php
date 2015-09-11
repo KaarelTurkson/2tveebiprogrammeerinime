167369fa8f44bd1a1387ebea4371c5c90ba856b9
Kaarel Türkson <kaartur@greeny.cs.tlu.ee>
2015-09-11 11:45:39 +0300

    Added logic

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,18 @@
 	
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+	$age=21;
+	// kui on väiksem kui 18 siis alaealine
+	// kui on suurem või võrdne siis täisealine
+	if($age <18) {
+	//tõene
+	echo "alaealine";
+	} else {
+	//väär
+	echo "täisealine";
+	}
+	
+	
+?>
